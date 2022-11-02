30c975f506aa76144e54a8fe4ea80f5431b63ea5
Jordan Wolter <jordan.wolter@icloud.com>
2022-11-02 16:08:20 -0500

    db up

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,7 +1,34 @@
 const pg = require('pg');
 
-let pool = new pg.Pool({
-  database: "cloud_to_do"
+//DATABASE_HOST=ramirez-db.cwp5rcbcqedy.us-east-1.rds.amazonaws.com
+
+
+
+// let pool;
+
+// if(process.env.DATABASE_URL){
+//   pool = new pg.pool({
+//     connectionString: process.env.DATABASE_URL
+//   })
+// }
+
+let pool;
+if (process.env.DATABASE_URL) {
+  pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL
+  })
+}
+else {
+  pool = new pg.Pool({
+    database: "cloud-todo",      // localhost
+  });
+}
+
+module.exports = pool;
+
+pool = new pg.Pool({
+  database: "cloud-todo",
+  host: process.env.DATABASE_HOST || 'localhost'
 });
 
 module.exports = pool;
